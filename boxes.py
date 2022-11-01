5210d7092566bf82224d0a8f3ca47ede4d3b909a
lmsorenson <lsorenson20@gmail.com>
2022-10-31 21:15:48 -0500

    commit files

--- boxes.py
+++ boxes.py
@@ -13,19 +13,19 @@ print("\r\nLoading File: " + filename + "\r\n\r\n")
 bpy.ops.wm.open_mainfile(filepath=filename + ".blend")
 bpy.ops.wm.save_as_mainfile(filepath=filename + "_boxes" + ".blend")
 
-for collection in [ bpy.data.collections[6] ]:
+for collection in [ bpy.data.collections[7] ]:
    print(collection.name)
-   new_hulls = []
+   new_boxes = []
    for obj in [ collection.all_objects[0] ]:
        if obj.type == "MESH":
            hull = minimal_bounding_box(obj)
-           new_hulls.append(hull)
+           new_boxes.append(hull)
            bpy.ops.wm.save_mainfile()
 
    new_collection = bpy.data.collections.new(collection.name + "_boxes")
-   for hull in new_hulls:
-       if hull.type == "MESH":
-           collection.objects.link(hull)
+   for bounding_box in new_boxes:
+       if bounding_box.type == "MESH":
+           collection.objects.link(bounding_box)
            bpy.ops.wm.save_mainfile()
    bpy.context.scene.collection.children.link(new_collection)
 
